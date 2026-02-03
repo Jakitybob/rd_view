7702c74efd0ec3e5dee83df523d13e203b165402
Jacob <jakitybob@gmail.com>
2026-02-03 01:00:51 -0600

    Created global_variables.h to contain static variables pertaining to things like the draw color. Ideally I would not have used it like this but given how the library code is obfuscated and normal methods were not working this was the approach I found to work.

---
+++ include/global_variables.h
@@ -0,0 +1,14 @@
+#ifndef CSCI631_PROJECT1_GLOBAL_VARIABLES_H
+#define CSCI631_PROJECT1_GLOBAL_VARIABLES_H
+
+// Global variables for the RGB draw color, default white
+static float drawRed = 1.0f;
+static float drawGreen = 1.0f;
+static float drawBlue = 1.0f;
+
+// Global variables for the RGB background color, default black
+static float backgroundRed = 0.0f;
+static float backgroundGreen = 0.0f;
+static float backgroundBlue = 0.0f;
+
+#endif //CSCI631_PROJECT1_GLOBAL_VARIABLES_H
--- src/rd_direct.cpp
+++ src/rd_direct.cpp
@@ -5,6 +5,7 @@
 
 #include "rd_display.h"
 #include "rd_error.h"
+#include "global_variables.h"
 
 int REDirect::rd_display(const string &name, const string &type, const string &mode)
 {
@@ -61,16 +62,32 @@ int REDirect::rd_line(const float start[3], const float end[3])
 
 int REDirect::rd_point(const float p[3])
 {
+    // Draw a single pixel at the desired location with the current draw color
+    rd_write_pixel(p[0], p[1], new float[3] {drawRed, drawGreen, drawBlue});
+
     return RD_OK;
 }
 
 int REDirect::rd_background(const float color[])
 {
+    // Store the color into our global variables
+    backgroundRed = color[0];
+    backgroundGreen = color[1];
+    backgroundBlue = color[2];
+
+    // Update the background to use our new color
+    rd_set_background(new float[3] {backgroundRed, backgroundGreen, backgroundBlue});
+
     return RD_OK;
 }
 
 int REDirect::rd_color(const float color[])
 {
+    // Store the colors into our global variables
+    drawRed = color[0];
+    drawGreen = color[1];
+    drawBlue = color[2];
+
     return RD_OK;
 }
 
