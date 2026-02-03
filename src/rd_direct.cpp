30d9bb1ef93db48b936d0b87ddc5e581bd495e7a
Jacob <jakitybob@gmail.com>
2026-02-03 01:08:54 -0600

    Added comments to rd_direct.cpp for my own sanity and tidiness.

--- src/rd_direct.cpp
+++ src/rd_direct.cpp
@@ -7,30 +7,33 @@
 #include "rd_error.h"
 #include "global_variables.h"
 
+// Simple stub as functionality is handled behind the scenes currently.
 int REDirect::rd_display(const string &name, const string &type, const string &mode)
 {
-
-
     return RD_OK;
 }
 
+// Simple stub as functionality is handled behind the scenes currently.
 int REDirect::rd_format(int xresolution, int yresolution)
 {
     return RD_OK;
 }
 
+/// Initializes the display for a new frame.
 int REDirect::rd_world_begin()
 {
     rd_disp_init_frame(frameNumber);
     return RD_OK;
 }
 
+/// Ensures that the last frame is ended properly.
 int REDirect::rd_world_end()
 {
     rd_disp_end_frame();
     return RD_OK;
 }
 
+/// Stores the current frame number out for later.
 int REDirect::rd_frame_begin(int frame_no)
 {
     // Store the frame number globally for use elsewhere
@@ -39,12 +42,14 @@ int REDirect::rd_frame_begin(int frame_no)
     return RD_OK;
 }
 
+/// Ensures that the last frame ends properly.
 int REDirect::rd_frame_end()
 {
     rd_disp_end_frame();
     return RD_OK;
 }
 
+/// TODO: Implement
 int REDirect::rd_circle(const float center[3], float radius)
 {
     return RD_OK;
@@ -60,6 +65,9 @@ int REDirect::rd_line(const float start[3], const float end[3])
     return RD_OK;
 }
 
+/// Draws a single pixel onto the screen at the desired x, y location.
+/// @param p A float* to an array of 3 values representing the x, y, and z coordinate of the point.
+///         Note that the z value is currently ignored.
 int REDirect::rd_point(const float p[3])
 {
     // Draw a single pixel at the desired location with the current draw color
@@ -68,6 +76,9 @@ int REDirect::rd_point(const float p[3])
     return RD_OK;
 }
 
+/// Updates the global RGB color values for the background color and forces an update to
+/// the background color with the new color as well.
+/// @param color A float* to an array of (currently) three values representing RGB.
 int REDirect::rd_background(const float color[])
 {
     // Store the color into our global variables
@@ -81,6 +92,8 @@ int REDirect::rd_background(const float color[])
     return RD_OK;
 }
 
+/// Updates the global RGB color values for drawing to the screen.
+/// @param color A float* to an array of (currently) three values representing RGB.
 int REDirect::rd_color(const float color[])
 {
     // Store the colors into our global variables
@@ -91,6 +104,7 @@ int REDirect::rd_color(const float color[])
     return RD_OK;
 }
 
+/// TODO: Implement
 int REDirect::rd_fill(const float seed_point[3])
 {
     return RD_OK;
