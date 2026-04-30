386b9c8408aef0447567335a3bc885b2d3e183eb
Jacob <jakitybob@gmail.com>
2026-04-30 18:38:11 -0500

    Added rd_lights.h which contains structs for the three lights. Added tons of new global variables in preparation for lighting.

--- include/global_variables.h
+++ include/global_variables.h
@@ -1,7 +1,7 @@
 /********************************************************************
-CSCI 490J - Assignment 2 - Spring 2026
+CSCI 490J - Assignment 5 - Spring 2026
 Programmer: Jacob Yates
-Date Due: 03/16/2026
+Last Updated: 4/30/2026
 
 Purpose: This file contains static global variables for the program
         to access during runtime and keeps information like the
@@ -15,8 +15,14 @@ Purpose: This file contains static global variables for the program
 #include "rd_xform.h"
 #include "rd_pointc.h"
 #include "rd_pointh.h"
+#include "rd_lights.h"
+#include "rd_pointa.h"
 
+#define MAX_LIGHTS 30
+
+// Forward declaration(s)
 struct rd_edge;
+
 // RGB draw color, default white
 static float drawRed = 1.0f;
 static float drawGreen = 1.0f;
@@ -48,6 +54,20 @@ static int NUM_SEGMENTS = 20; // The number of segments to use for round objects
 // The depth buffer for handling z-based depth when drawing pixels
 static float** depth_buffer = nullptr; // A two-dimensional array
 static rd_edge* edge_table = nullptr; // The table for each edge in a scanline
+
+// Lighting-related globals
+static rd_ambient_light ambient_light;
+static rd_far_light far_lights[MAX_LIGHTS];
+static rd_point_light point_lights[MAX_LIGHTS];
+static float ambient_coefficient = 1.f, diffuse_coefficient = 0.f, specular_coefficient = 0.f;
+static float surface_color[3] = {1.f, 1.f, 1.f}; // Default to white
+static float specular_color[3] = {1.f, 1.f, 1.f}; // Default to white
+static float specular_exponent = 10.f;
+static bool vertex_color_flag = false, vertex_normal_flag = false, vertex_texture_flag = false, interpolation_flag = true;
+static rd_vector viewing_vector; // Direction from a surface point to the eye of the camera for a given polygon
+static rd_vector poly_normal; // Surface normal for a given polygon, may or may not be used
+static rd_pointa surface_point_values;
+// TODO: add shader function ptr here
 
 // Global variable for frame number
 static int frame_number = 0;
